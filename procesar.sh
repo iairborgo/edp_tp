eb8e970c9f0f5e1cd4bb0e3a9ffeb909796f506e
Iair Borgo Elgart <105130558+iairborgo@users.noreply.github.com>
2023-06-24 19:48:23 +0000

    Creacion de procesar, añadido de carpetas para mantener limpieza luego.

---
+++ procesar.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+REGEX="^/[A-Z][a-z ]+$"
+DIRECTORIO="$( cd "$( dirname "$0" )" && pwd )"
+IMAGENES="$DIRECTORIO/fotos_descomprimidas"
+PROCESADA="$DIRECTORIO/fotos_procesadas"
+
+# Primero checkeemos que haya imagenes para procesar
+
+[ "$(ls -A $IMAGENES)" ] || (echo "Directorio Vacío" && exit 1) 
+cd $IMAGENES
+LISTA="/*"
+
+for f in $LISTA
+do
+    if [[ $f =~ $REGEX ]]
+    then
+        convert $f -gravity center -resize 512x512+0+0 \
+        -extent 512x512 "$PROCESADA/f"
+    fi
+done

2fc6b2adf6ffac27391d88a49ec6581ece8c7f2a
anardi <nardi_71_7@hotmail.com>
2023-07-09 17:50:26 -0300

    Actualizacion del script menu.sh agregado de saltos de linea y parametrización para la opcion generar

--- menu.sh
+++ menu.sh
@@ -7,8 +7,9 @@ PS3='Elija una opción de la lista> ' 							#En este prompt se guarda un mensaj
 
 select opciones in 'generar' 'descomprimir' 'procesar' 'comprimir' 'salir'$'\n'
 do
-	[ -e $opciones ] && echo "La opcion ingresada no es correcta!" && continue 	# Check por si la opcion ingresada no es válida se indica el mensaje y continúa
+	[ -e $opciones ] && echo -e "\nLa opcion ingresada no es correcta!\n" && continue 	# Check por si la opcion ingresada no es válida se indica el mensaje y continúa
 	[ $REPLY == 5 ] && echo -e "\nSaludos!\n" && break					# En REPLY tenemos almacenada la entrada del usuario, en caso de ser 5 termina el programa
-	[ $REPLY == 1 ] || [ $REPLY == 2 ] || [ $REPLY == 3 ] || [ $REPLY == 4 ] && ./$opciones.sh && continue	# Chequeo por el resto de opciones ejecutando los scripts en caso de matchear
+	[ $REPLY == 1 ] && read -p "Ingrese la cantidad de imágenes que desea descargar:" cant_imags && ./$opciones.sh "$cant_imags" && continue 	#Solicita al usuario el ingreso de un numero para pasar por parámetro al script de generar 
+	[ $REPLY == 2 ] || [ $REPLY == 3 ] || [ $REPLY == 4 ] && ./$opciones.sh && continue	# Chequeo por el resto de opciones ejecutando los scripts en caso de matchear
 done
 exit 0

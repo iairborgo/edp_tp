c391e6fb9bf9f79e28686ccd25ca1ea7c8a1b510
anardi <nardi_71_7@hotmail.com>
2023-07-15 18:05:14 -0300

    Modificaciones en menu para opciones Generar y Descomprimir

--- menu.sh
+++ menu.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 
-#Script del menu navegable
+#Script del menú navegable
+intentos_gen=0
+intentos_des=0
+continua_gen=true
+continua_des=true
 
 echo -e "\nLista de opciones: \n"
-PS3='Elija una opción de la lista> ' 							#En este prompt se guarda un mensaje para mostrar al usuario al momento de ingresar una opcion del select
+
+PS3='Elija una opción de la lista> '
 
 select opciones in 'generar' 'descomprimir' 'procesar' 'comprimir' 'salir'$'\n'
 do
-	[ -e $opciones ] && echo -e "\nLa opcion ingresada no es correcta!\n" && continue 	# Check por si la opcion ingresada no es válida se indica el mensaje y continúa
-	[ $REPLY == 5 ] && echo -e "\nSaludos!\n" && break					# En REPLY tenemos almacenada la entrada del usuario, en caso de ser 5 termina el programa
-	[ $REPLY == 1 ] && read -p "Ingrese la cantidad de imágenes que desea descargar:" cant_imags && ./$opciones.sh "$cant_imags" && continue 	#Solicita al usuario el ingreso de un numero para pasar por parámetro al script de generar 
-	[ $REPLY == 2 ] || [ $REPLY == 3 ] || [ $REPLY == 4 ] && ./$opciones.sh && continue	# Chequeo por el resto de opciones ejecutando los scripts en caso de matchear
+    [ -z "$opciones" ] && echo -e "\nLa opción ingresada no es correcta!\n" && continue
+    [ "$REPLY" == 5 ] && echo -e "\nSaludos!\n" && break
+    [ "$REPLY" == 1 ] && 
+        while [ $intentos_gen -lt 5 ] && $continua_gen # Setearemos un máximo de 5 intentos
+        do
+            read -p "Ingrese la cantidad de imágenes que desea descargar: " cant_imags  # Lee el ingreso del usuario almacenandolo en la variable
+            ./$opciones.sh "$cant_imags"    # Ejecuta el script de generar con el argumento de la variable anterior
+            if [ $? -eq 0 ] 
+            then 
+                continua_gen=false
+            else
+                intentos_gen=$((intentos_gen + 1))
+                if [ $intentos_gen -ge 5 ] 
+                then 
+                    echo -e "\nHas intentado demasiadas veces..\n"
+                fi 
+            fi
+        done
+    [ "$REPLY" == 2 ] &&
+        while [ $intentos_des -lt 5 ] && $continua_des
+        do
+            read -p "Ingrese primero el archivo de imágenes comprimidas y luego el checksum en orden: " zip sum
+            ./$opciones.sh "$zip" "$sum"
+	    if [ $? -eq 0 ]
+	    then
+		continua_des=false
+	    else
+            	intentos_des=$((intentos_des + 1))
+            	if [ $intentos_des -ge 5 ]
+            	then
+                	echo -e "\nHas intentado demasiadas veces..\n"
+            	fi
+	    fi
+        done
+    [ "$REPLY" == 3 ] || [ "$REPLY" == 4 ] && ./$opciones.sh && continue
 done
+
 exit 0
